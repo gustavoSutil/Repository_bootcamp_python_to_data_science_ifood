eb97b98d59d8c74739ec2fe7c63c86144ec9356a
Gustavo Guilherme Sutil Alberton <gustavo.alberton@expressosaomiguel.com.br>
2023-08-10 17:14:23 -0300

    fix: mudando a estrutura da pasta

--- sistema_bancario/sistema_bancario_desafio2/classes/DateController.py
+++ sistema_bancario/sistema_bancario_desafio2/classes/DateController.py
@@ -1,10 +1,14 @@
 import datetime
 class DateController():
-    def registerMoment() -> datetime.datetime:
-        return datetime.datetime.now()
-    def formatedDate(moment = registerMoment()) -> str:
-        dia = moment.day
-        mes = moment.month
-        ano = moment.year
-        hora = moment.time()
+    def __init__(self):
+        self.date = datetime.datetime.now()
+    def registerMoment(self) -> datetime.datetime:
+        DateController()
+        return self.date
+    def formatedDate(self) -> str:
+        moment = DateController()
+        dia = moment.date.day
+        mes = moment.date.month
+        ano = moment.date.year
+        hora = moment.date.time()
         return f'{dia}/{mes}/{ano} às {hora}'
